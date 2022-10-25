299eed67e06e776e07ef6ed77bc7883b45936e56
yakupulu <110382043+Yakupulu@users.noreply.github.com>
2022-10-26 00:07:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("merhaba yakup")
+
+//js type safe değildir!!!
+
+let dolarDun=9.30
+let dolarBugun=9.20
+
+console.log(dolarDun)
+
+let konutKredileri=["konut","emlak","kamu","özel"]
+
+console.log("<ul>")
+for(let i=0    ; i<konutKredileri.length   ;   i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
